6492031118bd64ce712d24795c296b8412d2ae63
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-08-15 19:12:50 +0530

    Add SearchBar component

--- weatherApp/src/App.jsx
+++ weatherApp/src/App.jsx
@@ -2,12 +2,22 @@ const App = () => {
   return (
     <div>
       <Header />
+      <SearchBar />
     </div>
   );
 };
 
 const Header = () => {
   return <h1>Weather App</h1>;
+};
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button type="submit">Search</button>
+    </div>
+  );
 };
 
 export default App;
